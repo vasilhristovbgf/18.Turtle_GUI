e154ed0681fccea5f8323e347ab8a0b121ec1f62
VasilHristov <vasil.hristovbgf@gmail.com>
2023-03-12 14:44:27 +0100

    Task 2: Shapes

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 import turtle
+from random import randint
 
-#task 1: Draw a Square
-
+turtle.colormode(255)
 tim = turtle.Turtle()
+# #task 1: Draw a Square
+#
+# for i in range(4):
+#     tim.forward(100)
+#     tim.right(90)
+#
+# turtle.exitonclick()
 
-for i in range(4):
-    tim.forward(100)
-    tim.right(90)
+tim.pensize(5)
+sides = 11
+while sides >= 3:
+    tim.pencolor(randint(1, 255),
+                 randint(1, 255),
+                 randint(1, 255))
+    for i in range(sides):
+        tim.begin_fill()
+        tim.forward(75)
+        tim.right(360/sides)
+        tim.end_fill()
+    sides -= 1
 
 turtle.exitonclick()
